3103cfac1fa13e7c9bbdee6ad6127732012a71c7
thangvdaltplus <thang.vd@altplus.com.vn>
2015-11-25 11:56:54 +0700

    upsert

--- myapp.rb
+++ myapp.rb
@@ -56,8 +56,8 @@ class MyApp < Sinatra::Base
     # new_employee << {'Name' => params[:fullname], 'username__c' => params[:username], 'Address__c' => params[:address], 'birthday__c'=> params[:dateofb], 
     #   'Hiredate__c' => params[:hiredate], 'Phone__c' => params[:phone], 'email__c'=> params[:email], 'Position__c' => params[:position], 'Team__c'=> params[:team]}
     # result = client.create('Employees__c', new_employee)
-    result = client.create('Employees__c', Name: params[:fullname], username__c: params[:username], Address__c: params[:address], birthday__c: params[:dateofb], 
-      Hiredate__c: params[:hiredate], Phone__c: params[:phone], email__c: params[:email], Position__c: params[:position], Team__c: params[:depart])
+    result = client.upsert('Employees__c', 'Team__c', Name: params[:fullname], username__c: params[:username], Address__c: params[:address], birthday__c: params[:dateofb], 
+      Hiredate__c: params[:hiredate], Phone__c: params[:phone], email__c: params[:email], Position__c: params[:position], Team__c: params[:team])
     if result
       redirect '/'
     else
