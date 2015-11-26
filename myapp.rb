470b45ae9d3df51a5da63fb9a7ee1f7b0f64ff35
thangvdaltplus <thang.vd@altplus.com.vn>
2015-11-26 11:08:51 +0700

    remove r

--- myapp.rb
+++ myapp.rb
@@ -57,7 +57,7 @@ class MyApp < Sinatra::Base
     #   'Hiredate__c' => params[:hiredate], 'Phone__c' => params[:phone], 'email__c'=> params[:email], 'Position__c' => params[:position], 'Team__c'=> params[:team]}
     # result = client.create('Employees__c', new_employee)
     result = client.create('Employees__c', Name: params[:fullname], username__c: params[:username], Address__c: params[:address], birthday__c: params[:dateofb], 
-      Hiredate__c: params[:hiredate], Phone__c: params[:phone], email__c: params[:email], Position__c: params[:position], Team__r.Name: params[:team])
+      Hiredate__c: params[:hiredate], Phone__c: params[:phone], email__c: params[:email], Position__c: params[:position], Team__r: params[:team])
     if result
       redirect '/'
     else
